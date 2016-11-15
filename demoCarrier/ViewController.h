95783ab153ac4b2a5d8b978c2e16fcea0f1feabe
Axel Kee <axelshaded@gmail.com>
2016-11-15 16:03:03 +0800

    first commit

--- demoCarrier/ViewController.h
+++ demoCarrier/ViewController.h
@@ -8,7 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *carrierNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *isoCountryCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mobileCountryCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mobileNetworkCodeLabel;
 
 
 @end
--- demoCarrier/ViewController.m
+++ demoCarrier/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Reachability.h"
+#import<CoreTelephony/CTCallCenter.h>
+#import<CoreTelephony/CTCall.h>
+#import<CoreTelephony/CTCarrier.h>
+#import<CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <ifaddrs.h>
 
 @interface ViewController ()
 
@@ -25,5 +31,87 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)checkCarrierTapped:(id)sender {
+    Reachability *reachability = [Reachability reachabilityForInternetConnection];
+    [reachability startNotifier];
+    
+    NetworkStatus status = [reachability currentReachabilityStatus];
+    
+    
+    UIAlertController *noInternetAlert = [UIAlertController alertControllerWithTitle:@"Notice" message:@"No internet connection, please enable mobile data" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertController *wifiAlert = [UIAlertController alertControllerWithTitle:@"Notice" message:@"Please turn off wifi and use mobile data" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertController *noCellularCapabilityAlert = [UIAlertController alertControllerWithTitle:@"Notice" message:@"This device has no cellular capabilities" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction
+                         actionWithTitle:@"OK"
+                         style:UIAlertActionStyleDefault
+                         handler:^(UIAlertAction * action)
+                         {
+                             //Do some thing here
+                             [noInternetAlert dismissViewControllerAnimated:YES completion:nil];
+                             [wifiAlert dismissViewControllerAnimated:YES completion:nil];
+                             [noCellularCapabilityAlert dismissViewControllerAnimated:YES completion:nil];
+                         }];
+    
+    [noInternetAlert addAction:ok];
+    [wifiAlert addAction:ok];
+    [noCellularCapabilityAlert addAction:ok];
+    
+    if(![self hasCellularCapabilities]){
+        NSLog(@"No cellular capabilities");
+        [self presentViewController:noCellularCapabilityAlert animated:YES completion:nil];
+    }
+    else if(status == NotReachable)
+    {
+        //No internet
+        NSLog(@"No internet connection");
+        [self presentViewController:noInternetAlert animated:YES completion:nil];
+    }
+    else if (status == ReachableViaWiFi)
+    {
+        //WiFi
+        NSLog(@"Please turn off wifi and use mobile data");
+        [self presentViewController:wifiAlert animated:YES completion:nil];
+    }
+    else if (status == ReachableViaWWAN)
+    {
+        //3G
+        CTTelephonyNetworkInfo *myNetworkInfo = [[CTTelephonyNetworkInfo alloc] init];
+        CTCarrier *myCarrier = [myNetworkInfo subscriberCellularProvider];
+        
+        NSLog(@"Carrier carrierName is %@", myCarrier.carrierName);
+        NSLog(@"Carrier isoCountryCode is %@", myCarrier.isoCountryCode);
+        NSLog(@"Carrier mobileCountryCode is %@", myCarrier.mobileCountryCode);
+        NSLog(@"Carrier mobileNetworkCode is %@", myCarrier.mobileNetworkCode);
+        
+        self.carrierNameLabel.text = myCarrier.carrierName;
+        self.isoCountryCodeLabel.text = myCarrier.isoCountryCode;
+        self.mobileCountryCodeLabel.text = myCarrier.mobileCountryCode;
+        self.mobileNetworkCodeLabel.text = myCarrier.mobileNetworkCode;
+    }
+    
+}
+
+//http://stackoverflow.com/questions/7101206/know-if-ios-device-has-cellular-data-capabilities
+- (BOOL) hasCellularCapabilities {
+    struct ifaddrs * addrs;
+    const struct ifaddrs * cursor;
+    bool found = false;
+    if (getifaddrs(&addrs) == 0) {
+        cursor = addrs;
+        while (cursor != NULL) {
+            NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
+            if ([name isEqualToString:@"pdp_ip0"]) {
+                found = true;
+                break;
+            }
+            cursor = cursor->ifa_next;
+        }
+        freeifaddrs(addrs);
+    }
+    return found;
+}
 
 @end
